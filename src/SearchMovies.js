765d2e774c838bb390fc5271d0aaddf5c6636a81
Toye Ogundepo <ogundepo@gmail.com>
2021-05-01 15:58:27 -0500

    complete search function to fetch movie data.

--- src/SearchMovies.js
+++ src/SearchMovies.js
@@ -1,8 +1,28 @@
 import React from "react";
 
 function SearchMovies() {
+
+    const searchMovies = async (e) => {
+        e.preventDefault();
+        console.log("submitting")
+
+        const query = "Jurassic Park"
+
+        const url = `https://api.themoviedb.org/3/search/movie?api_key=ec600c302d27132b9fb5bf1617ffd8d8&language=en-US&query=${query}&page=1&include_adult=false`;
+
+        try {
+            const res = await fetch(url);
+        const data = await res.json();
+        console.log(data)
+        } catch(err) {
+            console.error(err);
+        }
+
+        
+    }
+
     return(
-        <form className={`form`}>
+        <form className={`form`} onSubmit={searchMovies}>
             <label className={`label`} htmlFor="query">Movie Name</label>
             <input className={`input`} type="text" name="query" placeholder="Search a movie..." />
             <button className={`button`} type="submit">Search</button>
