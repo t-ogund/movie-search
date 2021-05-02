6c16593952a01bc4f10b3425d7ed87ed87647f9f
Toye Ogundepo <ogundepo@gmail.com>
2021-05-02 15:39:31 -0500

    complete project.

---
+++ src/MovieCard.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+function MovieCard(props) {
+    console.log(props)
+    return(
+        <div className={`card-list`}>
+            {props.movies.filter(movie => movie.poster_path).map(movie => (
+                <div className={`card`} key={movie.id}>
+                    <img className={`card--image`} src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${movie.poster_path}`} 
+                        alt={movie.title + " poster"}
+                    />
+                    <div className={`card--content`}>
+                        <h3 className={`card-title`}>{movie.title}</h3>
+                        <p><small>RELEASE DATE: {movie.release_date}</small></p>
+                        <p><small>RELEASE RATING: {movie.vote_average}</small></p>
+                        <p className={`card--desc`}>{movie.overview}</p>
+                    </div>
+                </div>
+            ))}
+        </div>
+    )
+}
+
+export default MovieCard;
--- src/SearchMovies.js
+++ src/SearchMovies.js
@@ -1,19 +1,24 @@
-import React from "react";
+import React, {useState} from "react";
+import MovieCard from "./MovieCard";
 
 function SearchMovies() {
 
+    const [query, setQuery] = useState("");
+    const [movies, setMovies] = useState([]);
+
     const searchMovies = async (e) => {
         e.preventDefault();
-        console.log("submitting")
+        
 
-        const query = "Jurassic Park"
+        // const query = "Jurassic Park"
 
         const url = `https://api.themoviedb.org/3/search/movie?api_key=ec600c302d27132b9fb5bf1617ffd8d8&language=en-US&query=${query}&page=1&include_adult=false`;
 
         try {
             const res = await fetch(url);
         const data = await res.json();
-        console.log(data)
+        console.log(data);
+        setMovies(data.results)
         } catch(err) {
             console.error(err);
         }
@@ -22,11 +27,14 @@ function SearchMovies() {
     }
 
     return(
-        <form className={`form`} onSubmit={searchMovies}>
-            <label className={`label`} htmlFor="query">Movie Name</label>
-            <input className={`input`} type="text" name="query" placeholder="Search a movie..." />
-            <button className={`button`} type="submit">Search</button>
-        </form>
+        <React.Fragment>
+            <form className={`form`} onSubmit={searchMovies}>
+                <label className={`label`} htmlFor="query">Movie Name</label>
+                <input className={`input`} type="text" name="query" placeholder="Search a movie..." onChange={(e) => setQuery(e.target.value)} value={query} />
+                <button className={`button`} type="submit">Search</button>
+            </form>
+            <MovieCard movies={movies}/>
+        </React.Fragment>
     )
 }
 
